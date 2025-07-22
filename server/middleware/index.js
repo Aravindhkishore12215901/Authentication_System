4505fab9b051831e4292a4b9f0a70e54ae8fd226
Aravindh Kishore <aravindhkishore111@gmail.com>
2025-07-22 16:39:23 +0530

    fifth commit

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,18 +1,13 @@
 import cors from 'cors';
 import express from 'express';
 
-const corsOptions = {
+const setMiddleware = (app) => {
+  app.use(cors({
   origin: ['https://authentication-system-tau.vercel.app', 'http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
-};
-
-const setMiddleware = (app) => {
-  app.use(cors(corsOptions));
-
-  // 🔧 Explicitly handle preflight requests
-  app.options('*', cors(corsOptions));
-
+  }));
+  
   app.use(express.json());
 };
 
